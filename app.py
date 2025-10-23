b90ed7056f971b41d3bccb8e832a123dbf905e67
Wojciech Pietrzak <wojciech.pietrzak@microsoft.wsei.edu.pl>
2025-10-23 12:12:37 +0000

    Dodanie testowej zmiany dla PR

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_devops():
-    return 'Hello, DevOps! To moj projekt.'
+def hello_devops(ssssssssswqq
+    return 'Hello, DevOps! To moj projekt. Milego czytania'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
